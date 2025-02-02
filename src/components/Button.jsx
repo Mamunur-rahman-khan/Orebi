26cd348d011aa1e8adee309f40f4febf5ceed034
Mamunur-rahman-khan <dipto2500@gmail.com>
2025-02-03 02:05:27 +0600

    gmap added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({ btnName, className }) => {
     return (
-        <button className={`font-dm font-bold text-sm text-white px-24 py-6 ${className}`}>{btnName}</button>
+        <button className={`font-dm font-bold text-sm bg-slate-950 text-white px-24 py-6 ${className}`}>{btnName}</button>
     )
 }
 
--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -39,12 +39,16 @@ const Contacts = () => {
                         <FormText FormHead={'Email'} />
                         <input type="text" placeholder='Your email here' className='border-b-2 border-b-[#F0F0F0] font-dm text-sm py-3 w-[550px]' />
                     </div>
-                    <div className='mt-6'>
+                    <div className='mt-6 mb-7'>
                         <FormText FormHead={'Message'} />
                         {/* <input type="text" placeholder='Your email here' className='border-b-2 font-dm text-sm py-3 w-[550px]' /> */}
                         <textarea name="message" placeholder='Your message here' className='border-b-2 border-b-[#F0F0F0] font-dm text-sm pt-3 pb-20 w-[550px] resize-none' />
                     </div>
                     <Button btnName={'Post'} />
+
+                    <div className='w-full py-32'>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1291.164124777442!2d90.3788973041363!3d23.74867622810208!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b98387c333a3%3A0x3d6a16f33fef1da1!2zRGhhbm1vbmRpIC0gS2FsYWJhZ2FuIENvbm5lY3RpbmcgQnJpZGdlIOCmp-CmvuCmqOCmruCmqOCnjeCmoeCmvy3gppXgprLgpr7gpqzgpr7gppfgpr7gpqgg4Ka44KaC4Kav4KeL4KaXIOCmuOCnh-CmpOCngQ!5e0!3m2!1sen!2sbd!4v1738525257200!5m2!1sen!2sbd" width="1444" height="600" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
                 </section>
                 {/* Contacts page form part end */}
 
